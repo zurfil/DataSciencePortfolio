a5f118fc2ed8dd74d8dfe9988a066fca3d325aa7
zurfil <zurfil@tutamail.com>
2024-01-15 13:53:03 +0100

    removed redundant operations from drop_personal_data()

--- FIFA/datafoobars.py
+++ FIFA/datafoobars.py
@@ -2,17 +2,12 @@ import pandas as pd
 
 
 def drop_personal_data(df, dropage=True):
-    # Personal data can be considered anything outside on-field abilities, for example player's photo.
+    df = df.drop(["Photo", "Flag", "Club Logo", "Special", "International Reputation",
+                  "Body Type", "Real Face", "Joined", "Loaned From", "Contract Valid Until",
+                  "Release Clause", "ID", "Name", "Nationality", "Club", "Value", "Wage", "Jersey Number",
+                  "Height", "Weight", "Marking"], axis=1, inplace=False)
     if dropage:
-        df = df.drop(["Photo", "Flag", "Club Logo", "Special", "International Reputation",
-                      "Body Type", "Real Face", "Joined", "Loaned From", "Contract Valid Until",
-                      "Release Clause", "ID", "Name", "Nationality", "Club", "Value", "Wage", "Jersey Number",
-                      "Height", "Weight", "Marking", "Age"], axis=1, inplace=False)
-    elif not dropage:
-        df = df.drop(["Photo", "Flag", "Club Logo", "Special", "International Reputation",
-                      "Body Type", "Real Face", "Joined", "Loaned From", "Contract Valid Until",
-                      "Release Clause", "ID", "Name", "Nationality", "Club", "Value", "Wage", "Jersey Number",
-                      "Height", "Weight", "Marking"], axis=1, inplace=False)
+        df = df.drop(["Age"], axis=1, inplace=False)
     return df
 
 
