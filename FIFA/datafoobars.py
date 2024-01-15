21851305fd7756041b3624ff62d0004fd25c7039
zurfil <zurfil@tutamail.com>
2024-01-15 14:21:56 +0100

    bugfixes for datafoobars.py, integrated datafoobars into overall_pred.ipynb

--- FIFA/datafoobars.py
+++ FIFA/datafoobars.py
@@ -5,18 +5,22 @@ def drop_personal_data(df, dropage=True):
     df = df.drop(["Photo", "Flag", "Club Logo", "Special", "International Reputation",
                   "Body Type", "Real Face", "Joined", "Loaned From", "Contract Valid Until",
                   "Release Clause", "ID", "Name", "Nationality", "Club", "Value", "Wage", "Jersey Number",
-                  "Height", "Weight", "Marking"], axis=1, inplace=False)
+                  "Height", "Weight", "Marking", "Potential", "Best Position", "Best Overall Rating"],
+                 axis=1, inplace=False)
     if dropage:
         df = df.drop(["Age"], axis=1, inplace=False)
     return df
 
 
-def drop_categorical_data(df):
-    df = df.drop(["Preferred Foot", "Weak Foot", "Skill Moves", "Work Rate"], axis=1, inplace=False)
+def drop_categorical_data(df, dropposition=True):
+    df = df.drop(["Preferred Foot", "Weak Foot", "Skill Moves", "Work Rate", "DefensiveAwareness"],
+                 axis=1, inplace=False)
+    if dropposition:
+        df = df.drop(["Position"], axis=1, inplace=False)
     return df
 
 
 def drop_goalkeeper_data(df):
     df = df.drop(["GKDiving", "GKHandling","GKKicking", "GKPositioning", "GKReflexes"], axis=1, inplace=False)
-    df = df.drop(df[df["Position"] == "GK"].index, inplace=True)
+    df.drop(df[df["Position"] == "GK"].index, inplace=True)
     return df
